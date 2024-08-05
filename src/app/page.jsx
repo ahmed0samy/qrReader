2f0dec220700029f47135475703946c522302cfd
ahmed0samy <86538090+ahmed0samy@users.noreply.github.com>
2024-08-05 16:54:48 +0300

    main

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -6,27 +6,54 @@ import { useEffect, useState } from "react";
 export default function Home() {
   const [scanResult, setScanResult] = useState(null);
   const [state, setstate] = useState(false);
-
-  let html5Qrcode;
+  // useEffect(() => {
+  //   const scanner = new Html5QrcodeScanner("reader", {
+  //     qrbox: {
+  //       width: 250,
+  //       height: 250,
+  //     },
+  //     fps: 30,
+  //     rememberLastUsedCamera: true,
+  //     useBarCodeDetectorIfSupported: true,
+  //   });
+  //   scanner.render(
+  //     (result) => {
+  //       scanner.clear();
+  //       setScanResult(result);
+  //       console.log("done succesfully");
+  //     },
+  //     (err) => {
+  //       console.log(er);
+  //     }
+  //   );
+  // });
   useEffect(() => {
-      html5Qrcode = new Html5Qrcode("reader");
-      const qrCodeSuccessCallback = (decodedText, decodedResult) => {
-        if (decodedText) {
-          document.getElementById("result").textContent = decodedText;
-        }
+    const html5Qrcode = new Html5Qrcode("reader");
+    const qrCodeSuccessCallback = (decodedText, decodedResult) => {
+      if (decodedText) {
+        document.getElementById("result").textContent = decodedText;
+        html5Qrcode.stop();
+      }
+      setstate(false);
+    };
+    const properties = [
+      { facingMode: "environment" },
+      { fps: 10, qrbox: { width: 700, height: 700 }, aspectRatio: 1, videoConstraints: {advanced: [{zoom: 3.0}]} },
+      qrCodeSuccessCallback
+    ];
+    // console.log(html5Qrcode.getState())
+    html5Qrcode.start(...properties);
+    document.getElementById("btn").addEventListener("click", () => {
+      if (html5Qrcode.isScanning) {
         html5Qrcode.stop();
         setstate(false);
-      };
-      html5Qrcode.start(
-        { facingMode: "environment" },
-        { fps: 6, qrbox: { width: 500}, aspectRatio: 1,},
-        qrCodeSuccessCallback
-      );
-      setstate(true);
+      } else {
+        html5Qrcode.start(...properties);
+        setstate(true);
+      }
+    });
+    setstate(true);
   }, []);
-  function stop() {
-    html5Qrcode.stop();
-  }
   return (
     <>
       <div className="container">
@@ -34,7 +61,7 @@ export default function Home() {
         <div id="result"></div>
         {/* <div id="result">{scanResult? scanResult : 'No Data Here'}</div> */}
         <h1 id="reader"> </h1>
-        <button onClick={stop}>stop</button>
+        <button id="btn">{state ? "Stop Scanning" : "Start Scanning"}</button>
       </div>
     </>
   );
