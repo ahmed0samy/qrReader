fba2bb601af0fb53f0218d0561cf43249d6ecf2a
ahmed0samy <86538090+ahmed0samy@users.noreply.github.com>
2024-08-11 11:10:11 +0300

    ..

--- src/app/new/operation/page.jsx
+++ src/app/new/operation/page.jsx
@@ -1,27 +1,59 @@
 "use client";
 import { Html5Qrcode, Html5QrcodeScanner } from "html5-qrcode";
-import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
-
+import styles from "./operation.module.scss";
+const data = [];
 export default function Home() {
-  const [scanResult, setScanResult] = useState([]);
+  const [scanResult, setScanResult] = useState([
+    {
+      name: "Bivatracin 150 ml",
+      price: 150,
+      code: "6224000588007",
+      count: "1",
+    },
+    {
+      name: "Bivatracin 150 ml Bivatracin 150 ml Bivatracin 150 ml",
+      price: 150,
+      code: "6224000588007",
+      count: "1",
+    },
+    {
+      name: "Bivatracin 150 ml",
+      price: 150,
+      code: "6224000588007",
+      count: "1",
+    },
+    {
+      name: "Bivatracin 150 ml",
+      price: 150,
+      code: "6224000588007",
+      count: "1",
+    },
+  ]);
+  let results = [];
   const [state, setstate] = useState(false);
-  const router = useRouter();
-  let code;
-  if (router.query){
-    ({ code } = router.query);
-  } else {
-    code = 'You are not redirected'
-  }
-
+  const code = window.sessionStorage.getItem("temp-code");
   useEffect(() => {
-    if (window){
+    if (window) {
+      if (code) {
+        setScanResult((prev) => [...prev, {code, name: 'placeholder', count: 0,}]);
+        results.push(code);
+        window.sessionStorage.removeItem("temp-code");
+      }
       const html5Qrcode = new Html5Qrcode("reader");
       const qrCodeSuccessCallback = (decodedText, decodedResult) => {
         if (decodedText) {
-          // document.getElementById("result").textContent = decodedText;
-          setScanResult((e) => [...e, decodedText])
-          html5Qrcode.stop();
+          console.log(
+            decodedText,
+            "should not be equal to",
+            results[results.length - 1]
+          );
+          if (decodedText != results[results.length - 1]) {
+            
+            setScanResult((e) => [{name: 'placeholder', price: '00', code: decodedText, count: 1}, ...e]);
+            results.push(decodedText);
+          }
+          // html5Qrcode.stop();
         }
         setstate(false);
       };
@@ -29,18 +61,16 @@ export default function Home() {
       if (window.localStorage.scanningCamera) {
         properties = [
           window.localStorage.scanningCamera,
-          { fps: 10, qrbox: { width: 700, height: 700 }, aspectRatio: 1,},
-          qrCodeSuccessCallback
+          { fps: 10, qrbox: { width: 300, height: 150 }, aspectRatio: 2 },
+          qrCodeSuccessCallback,
         ];
       } else {
         properties = [
           { facingMode: "environment" },
-          { fps: 10, qrbox: { width: 700, height: 700 }, aspectRatio: 1,},
-          qrCodeSuccessCallback
+          { fps: 10, qrbox: { width: 300, height: 150 }, aspectRatio: 2 },
+          qrCodeSuccessCallback,
         ];
       }
-      // console.log(html5Qrcode.getState())
-      console.log(properties)
       html5Qrcode.start(...properties);
       document.getElementById("btn").addEventListener("click", () => {
         if (html5Qrcode.isScanning) {
@@ -56,13 +86,31 @@ export default function Home() {
   }, []);
   return (
     <>
-      <div className="container">
-        <div>code is {code}</div>
+      <div className={`container ${styles.container}`}>
         <h1>{state ? "Camera is running" : "Camera is closed"}</h1>
-        <div id="result">{scanResult.map((e, i) => <p key={i}> {e} </p>)}</div>
-        {/* <div id="result">{scanResult? scanResult : 'No Data Here'}</div> */}
-        <h1 id="reader"> </h1>
+        <div id="result">
+          {scanResult.map((e, i) => (
+            <div className={`${styles.row} w-full bg-slate-100`} key={i}>
+              <span>{e.name}</span>
+              <span>{e.price}</span>
+              <span>{e.count}</span>
+              <span>{e.code}</span>
+            </div>
+          ))}
+        </div>
+        <div className={styles.reader}>
+          <h1 id="reader"> </h1>
+        </div>
         <button id="btn">{state ? "Stop Scanning" : "Start Scanning"}</button>
+
+        <br />
+        <button
+          onClick={() => {
+            window.location.assign("/");
+          }}
+        >
+          Done
+        </button>
       </div>
     </>
   );
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,37 +1,36 @@
 "use client";
 import { Html5Qrcode, Html5QrcodeScanner } from "html5-qrcode";
 import { useEffect, useState } from "react";
-import { useRouter } from 'next/navigation';
-import styles from './home.module.css'
+import styles from "./home.module.css";
 
 export default function Home() {
-    const router = useRouter();
-    function redirectForNewOperation(code) {
-      router.push({
-        pathname: '/new/operation',
-        query: {code}
-      });
+  function redirectForNewOperation(code) {
+    if (window) {
+      window.sessionStorage.setItem("temp-code", code);
+      window.location.replace("/new/operation");
     }
+  }
+
   useEffect(() => {
     const html5Qrcode = new Html5Qrcode("reader");
     const qrCodeSuccessCallback = (decodedText, decodedResult) => {
       if (decodedText) {
-        redirectForNewOperation(decodedText)
+        redirectForNewOperation(decodedText);
         html5Qrcode.stop();
       }
     };
     let properties;
-    if (window.localStorage.scanningCamera){
+    if (window.localStorage.scanningCamera) {
       properties = [
         window.localStorage.scanningCamera,
-        { fps: 10, qrbox: { width: 700, height: 700 }, aspectRatio: 1,},
-        qrCodeSuccessCallback
+        { fps: 10, qrbox: { width: 700, height: 700 }, aspectRatio: 1 },
+        qrCodeSuccessCallback,
       ];
     } else {
       properties = [
         { facingMode: "environment" },
-        { fps: 10, qrbox: { width: 700, height: 700 }, aspectRatio: 1,},
-        qrCodeSuccessCallback
+        { fps: 10, qrbox: { width: 700, height: 700 }, aspectRatio: 1 },
+        qrCodeSuccessCallback,
       ];
     }
     html5Qrcode.start(...properties);
@@ -39,7 +38,9 @@ export default function Home() {
   return (
     <>
       <div className="container">
-        <h1 className={styles.reader} id="reader"></h1>
+        <div>
+          <h1 className={styles.reader} id="reader"></h1>
+        </div>
       </div>
     </>
   );
